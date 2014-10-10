4a13e0e12236dba3d6f66307380ac723aad4d52b
TopShelfBullard <amb@amitchellbullard.com>
2014-10-09 22:51:23 -0500

    Use matching rather then 'when'

--- fizz_buzz.exs
+++ fizz_buzz.exs
@@ -1,19 +1,18 @@
 defmodule FizzBuzz do
   def get_remainders(n), do: {rem(n,3),rem(n,5),n}
 
-  def fizz_buzz({0,0,_}), do: IO.puts "FizzBuzz"   
-  def fizz_buzz({0,_,_}), do: IO.puts "Fizz"
-  def fizz_buzz({_,0,_}), do: IO.puts "Buzz"
-  def fizz_buzz({_,_,n}), do: IO.puts "#{n}"
-
-  def count_up_to_100_starting_at(number) when number < 100 do
+  def print_result({0,0,_}), do: IO.puts "FizzBuzz"   
+  def print_result({0,_,_}), do: IO.puts "Fizz"
+  def print_result({_,0,_}), do: IO.puts "Buzz"
+  def print_result({_,_,n}), do: IO.puts "#{n}"
+  
+  def fizz_buzz(101), do: IO.puts "Fin"
+  def fizz_buzz(number) do
     number
     |> get_remainders
-    |> fizz_buzz
-    count_up_to_100_starting_at(number + 1)
+    |> print_result
+    fizz_buzz(number + 1)
   end
-
-  def count_up_to_100_starting_at(number), do: IO.puts "Fin"
 end
 
-FizzBuzz.count_up_to_100_starting_at 1
+FizzBuzz.fizz_buzz 1
