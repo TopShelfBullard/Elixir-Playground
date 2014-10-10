d8b71d3751796e6b57328e7ef59407d2212cc0fa
TopShelfBullard <amb@amitchellbullard.com>
2014-10-09 23:11:27 -0500

    Add 'run' function and convert 'next_number' function to use matching

--- fibinacci_sequence.exs
+++ fibinacci_sequence.exs
@@ -1,13 +1,12 @@
 defmodule FibinacciSequence do
-  def next_number(number_one, number_two, to_the_nth) when to_the_nth > 0 do
+  def next_number(_, _, 0), do: IO.puts "fin"
+  def next_number(number_one, number_two, to_the_nth) do
     number_three = number_one + number_two 
     IO.puts number_three = number_one + number_two
     next_number(number_two, number_three, to_the_nth - 1)
   end
-
-  def next_number(_, _, 0), do: IO.puts "fin"
-
-  def next_number(to_the_nth), do: next_number(0, 1, to_the_nth)
+  
+  def run(to_the_nth), do: next_number(0, 1, to_the_nth)
 end
 
-FibinacciSequence.next_number(10000)
+FibinacciSequence.run(10000)
