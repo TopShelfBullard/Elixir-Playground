6ca0ebe7c7a8038aaba02cfd08af33e5201f5007
TopShelfBullard <amb@amitchellbullard.com>
2014-10-09 20:06:36 -0500

    Add namespaces

--- rock_paper_scissors.exs
+++ rock_paper_scissors.exs
@@ -1,18 +1,18 @@
 defmodule RockPaperScissors do
   def get_player_pick({player_score, computer_score}) do
-    parse_player_pick({player_score, computer_score, IO.gets(Output.choices)})
+    parse_player_pick({player_score, computer_score, IO.gets(RockPaperScissors.Output.choices)})
   end
 
   def parse_player_pick({player_score, computer_score, "1\n"}), do: {player_score, computer_score, 1}
   def parse_player_pick({player_score, computer_score, "2\n"}), do: {player_score, computer_score, 2}
   def parse_player_pick({player_score, computer_score, "3\n"}), do: {player_score, computer_score, 3}
   def parse_player_pick({player_score, computer_score, _}) do
-    Output.error
+    RockPaperScissors.Output.error
     get_player_pick({player_score, computer_score})
   end
 
   def get_computer_pick({player_score, computer_score, player_pick}) do
-    {player_score, computer_score, player_pick, Utilities.random_number(3)}
+    {player_score, computer_score, player_pick, AMB.Utilities.random_number(3)}
   end
 
   def get_result({player_score, computer_score, player_pick, computer_pick}) do
@@ -31,47 +31,43 @@ defmodule RockPaperScissors do
     {player_score + 1, computer_score, player_pick, computer_pick, "*** You won that round. ***"}
   end 
   
-  def play_game({5, _}), do: Output.player_ultimate_victory
-  def play_game({_, 5}), do: Output.computer_ultimate_victory
+  def play_game({5, _}), do: RockPaperScissors.Output.player_ultimate_victory
+  def play_game({_, 5}), do: RockPaperScissors.Output.computer_ultimate_victory
   def play_game({player_score, computer_score}) do
     get_player_pick({player_score, computer_score})
     |> get_computer_pick
     |> get_result
     |> adjust_score
-    |> Output.round_results
+    |> RockPaperScissors.Output.round_results
     |> play_game 
   end
 end
 
-defmodule Output do
+defmodule RockPaperScissors.Output do
   def welcome_message, do: IO.puts "\n*** Welcome to Rock, Paper, Scissors. ***\n\t- First one to 5 wins. -\n"
-  
   def choices, do: "Please choose 1, 2, or 3 and press enter: \n\t1 = rock\n\t2 = paper\n\t3 = scissors\n  : "
-  
   def error, do: IO.puts "INVALID INPUT: Please choose again."
-  
+  def player_ultimate_victory, do: IO.puts "You won the game! Good Job, Buddy!\n"
+  def computer_ultimate_victory, do: IO.puts "The computer just won the game. You aren't very good at this, are you?\n"
+
+  def parse_choice(1), do: "ROCK"
+  def parse_choice(2), do: "PAPER"
+  def parse_choice(3), do: "SCISSORS"
+
   def round_results({player_score, computer_score, player_pick, computer_pick, round_winner}) do
     IO.puts "\nYou chose: #{parse_choice(player_pick)}\nThe computer chose #{parse_choice(computer_pick)}.\n"
     IO.puts round_winner
     IO.puts "\nThe score is now:\n\tYou = #{player_score}\n\tComputer = #{computer_score}\n"
     {player_score, computer_score}
   end
-
-  def parse_choice(1), do: "ROCK"
-  def parse_choice(2), do: "PAPER"
-  def parse_choice(3), do: "SCISSORS"    
-
-  def player_ultimate_victory, do: IO.puts "You won the game! Good Job, Buddy!\n"
-  
-  def computer_ultimate_victory, do: IO.puts "The computer just won the game. You aren't very good at this, are you?\n" 
 end
 
-defmodule Utilities do
+defmodule AMB.Utilities do
   def random_number(number) do
     :random.seed(:erlang.now)
     :random.uniform(number)
   end
 end
 
-Output.welcome_message
+RockPaperScissors.Output.welcome_message
 RockPaperScissors.play_game({0,0})
