3361f5401d86a47f8dcefa2912a5af21700e1dd5
TopShelfBullard <amb@amitchellbullard.com>
2014-10-13 23:23:31 -0500

    Whitespace commit

--- fizz_buzz.exs
+++ fizz_buzz.exs
@@ -1,10 +1,10 @@
 defmodule FizzBuzz do
-  def get_remainders(n), do: {rem(n,3),rem(n,5),n}
+  def get_remainders(n), do: {rem(n,3), rem(n,5), n}
 
-  def print_result({0,0,_}), do: IO.puts "FizzBuzz"   
-  def print_result({0,_,_}), do: IO.puts "Fizz"
-  def print_result({_,0,_}), do: IO.puts "Buzz"
-  def print_result({_,_,n}), do: IO.puts "#{n}"
+  def print_result({0, 0, _}), do: IO.puts "FizzBuzz"   
+  def print_result({0, _, _}), do: IO.puts "Fizz"
+  def print_result({_, 0, _}), do: IO.puts "Buzz"
+  def print_result({_, _, n}), do: IO.puts "#{n}"
   
   def fizz_buzz(101), do: IO.puts "Fin"
   def fizz_buzz(number) do
